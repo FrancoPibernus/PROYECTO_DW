14a7c0fcec6adfa3b1a5e29d3621ed284677e685
Franco Pibernus <francopibe13@gmail.com>
2022-12-06 18:16:29 -0300

    Primera pre entrega JS

--- SCRIPTS.js
+++ SCRIPTS.js
@@ -1,10 +1,17 @@
+let cantidadNoches =parseInt(prompt("ingrese la cantidad de noches que desea quedarse"));
 const precioNoche = 6500;
-let cantidadNoches = prompt("ingrese la cantidad de noches que desea quedarse");
 
 let resultado = precioNoche * cantidadNoches;
 
+while (cantidadNoches <= "10"){
+    alert("El valor por una estadía de "+ cantidadNoches + " noches, es de $" + resultado + " pesos.");
+    break;
+}
+
 if (resultado > precioNoche * 10 ){
     alert("Ya que su reserva sería por mas de 10 días, se le aplicará un descuento del 10% al momento de reservar")
+    alert("El valor sin el descuento por una estadía de "+ cantidadNoches + " noches, es de $" + resultado + " pesos.");
 }
-alert("El valor por una estadía de "+ cantidadNoches + " noches, es de $" + resultado + " pesos.");
-  
+
+
+
