24204d08b72cb7a88519f1a2048a51f163f9c0b6
Franco Pibernus <francopibe13@gmail.com>
2022-12-12 15:44:03 -0300

    corrección primera entrega

--- SCRIPTS.js
+++ SCRIPTS.js
@@ -1,17 +1,30 @@
-let cantidadNoches =parseInt(prompt("ingrese la cantidad de noches que desea quedarse"));
-const precioNoche = 6500;
+function calcular(){
+    alert("A continuación indique la cantidad de noches que desea hospedarse en nuestro alojamiento");
 
-let resultado = precioNoche * cantidadNoches;
+    const precioNoche = 6500;
 
-while (cantidadNoches <= "10"){
+    let cantidadNoches =parseInt(prompt("ingrese la cantidad de noches que desea quedarse"));
+
+    let resultado = precioNoche * cantidadNoches;
+
+    while (cantidadNoches <= "10"){
     alert("El valor por una estadía de "+ cantidadNoches + " noches, es de $" + resultado + " pesos.");
     break;
-}
+    }
 
-if (resultado > precioNoche * 10 ){
+    if (resultado > precioNoche * 10 ){
     alert("Ya que su reserva sería por mas de 10 días, se le aplicará un descuento del 10% al momento de reservar")
+
     alert("El valor sin el descuento por una estadía de "+ cantidadNoches + " noches, es de $" + resultado + " pesos.");
+    }
 }
 
 
+function terminar(mensaje){
+    alert("Para consultar nuevamente por el valor de una estadía, actualice la página ");
 
+}
+
+
+calcular();
+terminar();
